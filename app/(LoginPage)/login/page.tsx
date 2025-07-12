8f123ea9f9614b4741d4da710e7d6a90d727665a
Karunakar_Patel <karunakarpatel.kp@gmail.com>
2025-07-12 11:41:20 +0530

    desktop work

--- app/(LoginPage)/login/page.tsx
+++ app/(LoginPage)/login/page.tsx
@@ -6,11 +6,17 @@ export const metadata: any = CustomMetaData({ presentURL: SEO_OBJ.HOME_PAGE.abso
 
 const LoginPage = () => {
   return (
-    <section className="grid grid-cols-12 border border-red-700 ">
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem libero officiis, similique repellendus
-      accusantium quod nihil, sed sapiente eligendi, aut a reiciendis?
-      {/* <section className="col-span-6 border border-black bg-brandColor ">One</section> */}
-      {/* <section className="col-span-6 border border-blue-800">Two</section> */}
+    <section className="grid grid-cols-12 border border-red-700  text-white h-full">
+      <section className="col-span-6 border border-black bg-brandColor">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum blanditiis incidunt asperiores. Neque eum, earum
+        distinctio! Saepe itaque dolores possimus libero necessitatibus voluptate ratione! Aspernatur earum ipsam
+        voluptas quia magni?
+      </section>
+      <section className="col-span-6 border border-blue-800 bg-blue-950 ">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione deserunt voluptatum, aperiam error veritatis
+        facere exercitationem nihil? Ea consectetur eum pariatur voluptatibus voluptatem libero, tempora quasi ad, nisi
+        ratione fugit debitis quo nam nesciunt repellat praesentium recusandae deserunt, iusto adipisci!
+      </section>
     </section>
   );
 };
